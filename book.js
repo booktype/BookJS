df131d8f3d51df8e105466ea01d9927ef3f2714a
Johannes Wilm <johannes@wilm.no>
2012-07-31 00:47:55 -0700

    new naming scheme, added simple TOC, hacked around Chrome bug with .overset

---
+++ book.js
@@ -0,0 +1,76 @@
+function romanize(num) {
+    if (!+num) return false;
+    var digits = String(+num).split(""),
+        key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+    roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+function setupDocument() {
+    $('body').wrapInner('<div id="contents" />');
+    $('body').append('<div id="layout" />');
+    $('#layout').append('<div class="page"><div class="contents"></div><div class="pagenumber">1</div></div>');
+}
+
+$(document).ready(function () {
+    setupDocument();
+    var hasEmptyPage = false;
+    var fillingPages = true;
+    var pageCounter = 1;
+    var namedFlow = document.webkitGetFlowByName("article");
+    while (fillingPages == true) {
+        if (namedFlow.firstEmptyRegionIndex == -1) { /* TODO: We use firstEmptyRegionIndex as overset gives us incorrect values in current Chromium. */
+            $('#layout').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + ++pageCounter + '</div></div>');
+        } else {
+            fillingPages = false;
+            $('#layout .page:last').detach();
+            pageCounter -= 1;
+        }
+    }
+    addFrontMatter();
+});
+
+function addFrontMatter() {
+    var romanPageCounter = 0;
+    var frontMatter = '';
+
+    function addFrontMatterPage(content) {
+        frontMatter += '<div class="page"><div class="frontmatter">' + content + '</div><div class="pagenumber">' + romanize(++romanPageCounter) + '</div></div>';
+    }
+
+    addFrontMatterPage('<h1>Booktitle</h1> <p>by Author</p>');
+
+    addFrontMatterPage(tocPage());
+
+    $('#layout').prepend(frontMatter);
+}
+
+function tocPage() {
+    var tocList = buildToc();
+    var tocContents = '<h2>Table of Contents</h2>';
+
+    $.each(tocList, function (index, headline) {
+        tocContents += '<div class="toc-entry">' + headline.text + ' ' + headline.pagenumber + '</div>';
+    });
+
+    return tocContents;
+}
+
+function buildToc() {
+    var namedFlow = document.webkitGetFlowByName("article");
+    var headlinePageList = [];
+    $('#contents h1').each(function () {
+        headlineContentDiv = namedFlow.getRegionsByContent(this)[0];
+        headlinePagenumber = $(headlineContentDiv).parent().find('.pagenumber').text();
+        headlineText = $(this).text();
+        headlinePageList.push({
+            'text': headlineText,
+            'pagenumber': headlinePagenumber
+        });
+    });
+    return headlinePageList;
+
+}
