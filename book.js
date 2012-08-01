dc137aa5c21e214a3d0ce03e6b3ce95e6c046732
Johannes Wilm <johannes@wilm.no>
2012-08-01 07:26:24 -0700

    beautified code

--- book.js
+++ book.js
@@ -1,7 +1,6 @@
 /*
  * (c) 2012 Johannes Wilm and Philip Schatz. Freely available under the AGPL. For further details see LICENSE.txt
  */
-
 var arabPageCounter = 1;
 var romanPageCounter = 1;
 
@@ -14,61 +13,61 @@ function setupDocument() {
     $('body').wrapInner('<div id="bodyraw" />');
     $('body').append('<div id="frontmatterraw" />');
     $('body').append('<div id="layout"><div id="frontmatter"/><div id="body"/></div>');
-    $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' +  arabPageCounter++ + '</div></div>');
-    $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' +  romanize(romanPageCounter++) + '</div></div>');
+    $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + arabPageCounter+++'</div></div>');
+    $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + romanize(romanPageCounter++) + '</div></div>');
 }
 
-    function addBodyPage() {
-        $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + arabPageCounter++ + '</div></div>');
-    }
+function addBodyPage() {
+    $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + arabPageCounter+++'</div></div>');
+}
 
-    function addFrontmatterPage() {
-        $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + romanize(romanPageCounter++) + '</div></div>');
-    }
-    
-    
-    // If text overflows from the region add more pages and then remove any empty ones
-    function addPagesIfNeeded(flowName,flowTo,pageCounter,addPageFunction) {
-        namedFlow = document.webkitGetFlowByName(flowName);
+function addFrontmatterPage() {
+    $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + romanize(romanPageCounter++) + '</div></div>');
+}
 
-        // TODO: We use firstEmptyRegionIndex as overset gives us incorrect values in current Chromium.
-        if (namedFlow.firstEmptyRegionIndex == -1) {
-            // Add several pages at a time
-            // console.log(new Date());
-            for (var i = 0; i < bulkPagesToAdd; i++) {
-              addPageFunction();
-            }
 
-            // The browser becomes unresponsive if we loop
-            // Instead, set a timeout
-            setTimeout(addPagesIfNeeded(flowName,flowTo,pageCounter,addPageFunction), 1);
-        } else {
-            // Remove the empty pages (up to bulkPagesToAdd - 1)
-            // (empty page needed to test if firstEmptyRegionIndex == -1 )
-            while (namedFlow.firstEmptyRegionIndex != -1) {
-              $('#'+flowTo+' .page:last').detach();
-              pageCounter -= 1;
-            }
+// If text overflows from the region add more pages and then remove any empty ones
+function addPagesIfNeeded(flowName, flowTo, pageCounter, addPageFunction) {
+    namedFlow = document.webkitGetFlowByName(flowName);
 
-           
-           
+    // TODO: We use firstEmptyRegionIndex as overset gives us incorrect values in current Chromium.
+    if (namedFlow.firstEmptyRegionIndex == -1) {
+        // Add several pages at a time
+        // console.log(new Date());
+        for (var i = 0; i < bulkPagesToAdd; i++) {
+            addPageFunction();
         }
+
+        // The browser becomes unresponsive if we loop
+        // Instead, set a timeout
+        setTimeout(addPagesIfNeeded(flowName, flowTo, pageCounter, addPageFunction), 1);
+    } else {
+        // Remove the empty pages (up to bulkPagesToAdd - 1)
+        // (empty page needed to test if firstEmptyRegionIndex == -1 )
+        while (namedFlow.firstEmptyRegionIndex != -1) {
+            $('#' + flowTo + ' .page:last').detach();
+            pageCounter -= 1;
+        }
+
+
+
     }
-    
+}
+
 $(document).ready(function () {
     setupDocument();
-    addPagesIfNeeded('body','body',arabPageCounter,addBodyPage);
-     //Done flowing pages; calculate the TOC
+    addPagesIfNeeded('body', 'body', arabPageCounter, addBodyPage);
+    //Done flowing pages; calculate the TOC
     addFrontMatter();
-    addPagesIfNeeded('frontmatter','frontmatter',romanPageCounter,addFrontmatterPage);
+    addPagesIfNeeded('frontmatter', 'frontmatter', romanPageCounter, addFrontmatterPage);
 });
 
 function addFrontMatter() {
-    
+
 
     $('#frontmatterraw').append('<h1>Booktitle</h1> <p>by Author</p><br class="pagebreak">');
     $('#frontmatterraw').append(tocPage());
-    
+
 }
 
 function tocPage() {
