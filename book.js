0b4ae4ce2676f6f0e6feba2bec2c0f5e8e73c64c
Johannes Wilm <johannes@wilm.no>
2012-08-02 05:32:20 -0700

    simplified a bit

--- book.js
+++ book.js
@@ -1,33 +1,46 @@
 /*
  * (c) 2012 Johannes Wilm and Philip Schatz. Freely available under the AGPL. For further details see LICENSE.txt
  */
-var arabPageCounter = 1;
-var romanPageCounter = 1;
+
+
 
 // Some timing stats: the time it takes to add each group (in seconds)
 // bulk adding 50 pages at a time: 7+10+12+19+24+37+22+40+44+48
 // bulk adding 100 pages at a time: 8+17+22+35+44
 var bulkPagesToAdd = 100; // For larger books add many pages at a time so there is less time spent reflowing text
 
+
+function pageCounter(show)
+{
+this.value=1;
+this.show = function() {
+if (show===undefined) {
+        return this.value;
+} else {
+        return show(this.value);
+}
+}
+}
+
+var arabPageCounter = new pageCounter();
+var romanPageCounter = new pageCounter(romanize);
+
+function addPage(flowTo,pageCounter) {
+    $('#'+flowTo).append('<div class="page"><div class="contents"></div><div class="pagenumber">' + pageCounter.show() + '</div></div>');
+    pageCounter.value++;
+}
+
 function setupDocument() {
     $('body').wrapInner('<div id="bodyraw" />');
     $('body').append('<div id="frontmatterraw" />');
     $('body').append('<div id="layout"><div id="frontmatter"/><div id="body"/></div>');
-    $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + arabPageCounter+++'</div></div>');
-    $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + romanize(romanPageCounter++) + '</div></div>');
-}
-
-function addBodyPage() {
-    $('#body').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + arabPageCounter+++'</div></div>');
-}
-
-function addFrontmatterPage() {
-    $('#frontmatter').append('<div class="page"><div class="contents"></div><div class="pagenumber">' + romanize(romanPageCounter++) + '</div></div>');
+    addPage('body',arabPageCounter);
+    addPage('frontmatter',romanPageCounter);
 }
 
 
 // If text overflows from the region add more pages and then remove any empty ones
-function addPagesIfNeeded(flowName, flowTo, pageCounter, addPageFunction) {
+function addPagesIfNeeded(flowName, flowTo, pageCounter) {
     namedFlow = document.webkitGetFlowByName(flowName);
 
     // TODO: We use firstEmptyRegionIndex as overset gives us incorrect values in current Chromium.
@@ -35,39 +48,33 @@ function addPagesIfNeeded(flowName, flowTo, pageCounter, addPageFunction) {
         // Add several pages at a time
         // console.log(new Date());
         for (var i = 0; i < bulkPagesToAdd; i++) {
-            addPageFunction();
+            addPage(flowTo, pageCounter);
         }
 
         // The browser becomes unresponsive if we loop
         // Instead, set a timeout
-        setTimeout(addPagesIfNeeded(flowName, flowTo, pageCounter, addPageFunction), 1);
+        setTimeout(addPagesIfNeeded(flowName, flowTo, pageCounter), 1);
     } else {
         // Remove the empty pages (up to bulkPagesToAdd - 1)
         // (empty page needed to test if firstEmptyRegionIndex == -1 )
         while (namedFlow.firstEmptyRegionIndex != -1) {
             $('#' + flowTo + ' .page:last').detach();
-            pageCounter -= 1;
+            pageCounter--;
         }
-
-
-
     }
 }
 
 $(document).ready(function () {
     setupDocument();
-    addPagesIfNeeded('body', 'body', arabPageCounter, addBodyPage);
-    //Done flowing pages; calculate the TOC
+    addPagesIfNeeded('body', 'body', arabPageCounter);
+    //Done flowing body pages; calculate the TOC
     addFrontMatter();
-    addPagesIfNeeded('frontmatter', 'frontmatter', romanPageCounter, addFrontmatterPage);
+    addPagesIfNeeded('frontmatter', 'frontmatter', romanPageCounter);
 });
 
 function addFrontMatter() {
-
-
     $('#frontmatterraw').append('<h1>Booktitle</h1> <p>by Author</p><br class="pagebreak">');
     $('#frontmatterraw').append(tocPage());
-
 }
 
 function tocPage() {
